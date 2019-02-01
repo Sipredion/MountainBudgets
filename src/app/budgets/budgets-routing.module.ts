fae5ec7ae9efbfa6f02557319d1cdf819d950b1b
Michael <mike@solinta.co.za>
2019-02-01 18:19:39 +0200

    Added budget list cmponent + route

--- src/app/budgets/budgets-routing.module.ts
+++ src/app/budgets/budgets-routing.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {BudgetListComponent} from './components/budget-list/budget-list.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
-      {path: 'dashboard', component: DashboardComponent}
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'list', component: BudgetListComponent}
     ]
   }
 ];
