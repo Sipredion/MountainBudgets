462ab19071e82e801d0bbe3f76bc42e732fd86a6
Michael <mike@solinta.co.za>
2019-02-15 18:17:46 +0200

    Add default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,11 @@ const routes: Routes = [
     path: 'budget',
     loadChildren: './budgets/budgets.module#BudgetsModule'
   },
-  // {
-  //   path: '',
-  //   pathMatch: 'full',
-  //   redirectTo: '/budget/list'
-  // }
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/budget/list'
+  }
 ];
 
 @NgModule({
